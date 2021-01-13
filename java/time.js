02056cd3b63e8ee88a9579c6e9fef1d3888b343b
erincampbell95 <cam20034@byui.edu>
2021-01-13 14:48:40 -0700

    changes

--- java/time.js
+++ java/time.js
@@ -1,6 +1,6 @@
 function dateUpdated(){
     var output = new Date(document.lastModified);
-    var date = newDate();
+    var date = new Date();
     var outputData = output;
 document.getElementById("copyRightYear").innerHTML = date.getFullYear();  
 document.getElementById("outputData").innerHTML = output;
