741c5e1b666545313134eaeef603ca49aa77ef86
erincampbell95 <cam20034@byui.edu>
2021-01-22 17:13:47 -0700

    changes

--- javascript/currentyear.js
+++ javascript/currentyear.js
@@ -1,3 +1,3 @@
 const thisYear = new Date();
 let year = thisYear.getFullYear();
-document.getElementById("currentYear").textContent = year;
+document.getElementById("currentYear").innerHTML = year;
