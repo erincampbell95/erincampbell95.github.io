ae5b287ffd137db795343a0c30ab6f1a0b6f5055
erincampbell95 <cam20034@byui.edu>
2021-02-05 16:10:39 -0700

    changes

--- lesson5.html/javascript/banner.js
+++ lesson5.html/javascript/banner.js
@@ -1,8 +1,6 @@
-function bannerAlert(){
     var now = new Date();
     var dayOfWeek = now.getDay(); 
 
     if (dayOfWeek == 5) {
-      document.getElementsByClassName("banner")[0].classList.toggle("responsive");
+      document.getElementsByClassName("banner")[0].classList.toggle("bannerShow");
    }
-}
