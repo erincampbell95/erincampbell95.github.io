c0644aad0990b48094cd4aab65866802a99d4b5a
erincampbell95 <cam20034@byui.edu>
2021-02-04 19:42:01 -0700

    changes

--- lesson5.html/javascript/banner.js
+++ lesson5.html/javascript/banner.js
@@ -3,7 +3,6 @@ function bannerAlert(){
     var month = now.getMonth();
     var dayOfMonth = now.getDate();
     var dayOfWeek = now.getDay(); 
-    let output;
 
     if (dayOfWeek == 4) {
       document.getElementsByClassName("banner")[0].classList.toggle("responsive");
