346201cdf93b13557fc2c56686d248810d938f4b
erincampbell95 <cam20034@byui.edu>
2021-02-24 13:29:47 -0700

    w9 home, wirframe

--- lesson9.html/javascript/home.js
+++ lesson9.html/javascript/home.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
 fetch(requestURL)
   .then(function (response) {
@@ -6,21 +6,23 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-    for (let i = 0; i < prophets.length; i++ ) {
+    for (let i = 0; i < towns.length; i++ ) {
     }
   });
 
-const prophets = jsonObject['prophets'];
+const towns = jsonObject['towns'];
 let card = document.createElement('section');
 let h2 = document.createElement('h2');
-let p = document.createElement('p');
+let h5 = document.createElement('h5');
+let h4 = document.createElement('h4');
 let image = document.createElement('img');
 
 h2.textContent = prophets[i].name + ' ' + prophets[i].lastname + "-"  + prophets[i.length-1];
 image.setAttribute('src', prophets[i].imageurl);
 
 card.appendChild(h2);
-card.appendChild(p);
+card.appendChild(h5);
+card.appendChild(h4);
 card.appendChild(image);
 
 document.querySelector('div.cards').appendChild(card);
