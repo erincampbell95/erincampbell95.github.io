c455a0256eff90bfa0682d53561280f92ce8ea04
erincampbell95 <cam20034@byui.edu>
2021-03-11 17:47:34 -0700

    weather stats

--- lesson10.html/javascript/weather.js
+++ lesson10.html/javascript/weather.js
@@ -1,12 +1,13 @@
 const apiURL_weather = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=b1c4009595630e2651d3712160a3c1f6&units=imperial';
 
-fetch(apiURL-weather)
+fetch(apiURL_weather)
   .then((response) => response.json())
   .then((town) => {
     console.log(town);
     let description = town.weather[0].description;
-    document.getElementById('currently').innerHTML = description.charAt(0).toUpperCase() + description.slice(1);
+    document.getElementById('current').innerHTML = description.charAt(0).toUpperCase() + description.slice(1);
     document.getElementById('current-temp').innerHTML = Math.round(town.main.temp);
     document.getElementById('humidity').innerHTML = town.main.humidity;
-    document.getElementById('windspeed').innerHTML = Math.round(town.wind.speed);
+    document.getElementById('windSpeed').innerHTML = Math.round(town.wind.speed);
+    windChill();
   });
