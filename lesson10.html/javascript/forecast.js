390545e7a0bc23f12a2bf44ac668540c6cf0019f
erincampbell95 <cam20034@byui.edu>
2021-03-11 17:23:35 -0700

    weather

--- lesson10.html/javascript/forecast.js
+++ lesson10.html/javascript/forecast.js
@@ -15,7 +15,7 @@ const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appi
     let x = new Date(forecast.dt_txt);
   document.getElementById('temp'+(day+1)).textContent = Math.round(forecast.main.temp) + ' °F';
   document.getElementById('img'+(day+1)).src = "https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
-  document.getElementById('img'+(day+1)).alt = forecast.weather[0].description
+  document.getElementById('img'+(day+1)).alt = forecast.weather[0].description;
   document.getElementById('day'+(day+1)).textContent = weekdays[x.getDay()];
   day++;	  
   });
