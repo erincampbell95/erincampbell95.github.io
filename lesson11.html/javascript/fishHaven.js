c5f9f7cf59c011736d40a4ab1a41f6a3ee68ee4f
erincampbell95 <cam20034@byui.edu>
2021-03-20 08:36:33 -0600

    css changes

--- lesson11.html/javascript/fishHaven.js
+++ lesson11.html/javascript/fishHaven.js
@@ -17,3 +17,19 @@ fetch(requestURL)
     }
    }
 });
+
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5585010&appid=b1c4009595630e2651d3712160a3c1f6&units=imperial';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
+
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+  });
+
